8f059a5f4017ba6081fb9f257105a768657f24a6
Juwon-Yun <eooll2@daum.net>
2021-12-02 16:03:27 +0900

    right databinding

--- src/components/Vue3_tree.vue
+++ src/components/Vue3_tree.vue
@@ -17,7 +17,7 @@ import Tree from 'vue3-tree'
 import "vue3-tree/dist/style.css";
 
 // name, type, file => content, dir => url, 
-const key = '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';
+const key = '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';
 
 export default {
     components : {
@@ -77,39 +77,42 @@ export default {
         }
     },
     methods: {
-        // onUpdate(e){
-        //     console.log(e)
-        // }
-        onUpdate(e) {
-            if(e.type === 'dir') {
-                this.axios.then(res => {
-                    for(let item of this.a) {
-                        if(item.idx === e.idx) {
-
-                            item.nodes.push(res.data)
-                        }
-                    }
-                })
-            } else if(e.type === 'file') {
-                this.axios(e.url).then( ()=>{
-                    // res.content.decodeBase64()
-                    // $store에 보내야함 
-                })
-                // $store.state.file = decodeURI(content)
-                return
-            }
-        },
         
+        onUpdate(e) {
+            this.axios.get(`${e.url}`, {
+                    headers : {
+                        Authorization : `token ${key}`
+                    }
+            })
+            .then( res => {
+                console.log(res)
+                for(let i of res.data){
+                    const a = {
+                        idx : i.sha,
+                        label : i.name,
+                        type : i.type,
+                        url : i.url,
+                        nodes : [],
+                        content : null,
+                    }
+                    if(i.type === 'file'){
+                        a.nodes = null
+                        a.content = i.content
+                    }
+
+                    e.nodes.push(a)
+                }
+            })
+        },
+
         getFileList() {
-            this.axios.get('https://api.github.com/repos/Juwon-Yun/Algorithm/contents', {
+            this.axios.get('https://api.github.com/repos/Juwon-Yun/kanboo_my_work/contents', {
                     headers : {
                         Authorization : `token ${key}`
                     }
                 })
             .then( res =>{
-
                 for(let i of res.data){
-                    
                     const arrayA = {
                         idx : i.sha,
                         label : i.name,
@@ -121,166 +124,12 @@ export default {
                     if(i.type === 'file'){
                         arrayA.nodes = null
                         arrayA.content = i.content
-                    }else if(i.type === 'dir'){
-                        this.axios.get(i.url, {
-                                headers : {
-                                    Authorization : `token ${key}`
-                                }
-                            }
-                        )
-                        .then( p_i_url => {
-                            for(let j of p_i_url.data){
-
-                                const arrayB = {
-                                    idx : j.sha,
-                                    label : j.name,
-                                    type : j.type,
-                                    url : j.url,
-                                    nodes : [],
-                                    content : null,
-                                }
-
-                                if(j.type === 'file'){
-                                    arrayB.nodes = null
-                                    arrayB.content = j.content
-
-                                }else if(j.type === 'dir'){
-                                    this.axios.get(j.url, {
-                                        headers : {
-                                            Authorization : `token ${key}`
-                                        }
-                                    }
-                                ).then( p_j_url => {
-                                        for(let k of p_j_url.data){
-
-                                            const arrayC = {
-                                                idx : k.sha,
-                                                label : k.name,
-                                                type : k.type,
-                                                url : k.url,
-                                                nodes : [],
-                                                content : null,
-                                            }
-
-                                            if(k.type === 'file'){
-                                                arrayC.nodes = null
-                                                arrayC.content = k.content
-                                            }else if(k.type === 'dir'){
-                                                this.axios.get(k.url, {
-                                                    headers : {
-                                                        Authorization : `token ${key}`
-                                                    }
-                                                }
-                                                ).then( p_k_url => {
-                                                        for(let l of p_k_url.data){
-
-                                                            const arrayD = {
-                                                                idx : l.sha,
-                                                                label : l.name,
-                                                                type : l.type,
-                                                                url : l.url,
-                                                                nodes : [],
-                                                                content : null,
-                                                            }
-
-                                                            if(l.type === 'file'){
-                                                                arrayD.nodes = null
-                                                                arrayD.content = l.content
-                                                            }
-
-                                                            else if(l.type === 'dir'){
-                                                                this.axios.get(l.url, {
-                                                                    headers : {
-                                                                        Authorization : `token ${key}`
-                                                                    }
-                                                                }
-                                                                ).then( p_l_url => {
-                                                                        for(let m of p_l_url.data){
-
-                                                                            const arrayE = {
-                                                                                idx : m.sha,
-                                                                                label : m.name,
-                                                                                type : m.type,
-                                                                                url : m.url,
-                                                                                nodes : [],
-                                                                                content : null,
-                                                                            }
-
-                                                                            if(m.type === 'file'){
-                                                                                arrayE.nodes = null
-                                                                                arrayE.content = m.content
-                                                                            }else if(m.type === 'dir'){
-                                                                                this.axios.get(l.url, {
-                                                                                    headers : {
-                                                                                        Authorization : `token ${key}`
-                                                                                    }
-                                                                                }
-                                                                                ).then( p_n_url => {
-                                                                                    for(let n of p_n_url.data){
-                                                                                        const arrayF = {
-                                                                                            idx : n.sha,
-                                                                                            label : n.name,
-                                                                                            type : n.type,
-                                                                                            url : n.url,
-                                                                                            nodes : [],
-                                                                                            content : null,
-                                                                                        }
-
-                                                                                        if(n.type === 'file'){
-                                                                                            arrayF.nodes = null
-                                                                                            arrayF.content = n.content
-                                                                                        }
-                                                                                        // else if(n.type === 'dir'){
-
-                                                                                        // }
-                                                                                        arrayE.nodes.push(arrayF)
-                                                                                    }// for of n
-                                                                                })// axios then
-                                                                            }// else 
-                                                                            
-                                                                            arrayD.nodes.push(arrayE)
-                                                                        }
-
-                                                                }) // p_l_url axios
-                                                            }
-
-                                                            arrayC.nodes.push(arrayD)
-                                                        }
-                                                }) // p_k_url axios
-                                            }
-
-                                            arrayB.nodes.push(arrayC)
-                                        }
-                                    })
-                                }
-
-                                arrayA.nodes.push(arrayB)
-                            }// for j of
-                        })
-                    }// url axios
+                    }
                     this.data.push(arrayA)
                 }// for i of
-
             })
         },
 
-        pushArrayToJson(data){
-          // 이시점에 json 배열로 변환후 state에 저장
-          console.log(data)
-          for(let i of data) {
-            const a = {
-              url: i.url,
-              label: i.name,
-              nodes: []
-            }
-            if( i.type ==='file'){
-              a.nodes = null
-            }
-            // state.gitrepo.push(a)
-          }
-
-        //   return state.gitrepo = data
-        },
     },
     mounted() {
         this.getFileList();
